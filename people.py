b06b0b736cd3b3bea0141fa222aded5a3eb4bf39
Nate Minty <nateminty@gmail.com>
2025-04-08 13:17:18 -0400

    Create people.py

---
+++ people.py
@@ -0,0 +1,66 @@
+import re
+
+
+
+def parse_name(text):
+
+    
+    
+
+
+
+
+    return #tuple (first, last)
+
+
+
+def parse_address(text):
+
+    #regex street city state 
+
+
+
+    
+    
+    addressobj = Address(street, city, state)
+
+    return addressobj
+
+
+
+def parse_email(text):
+
+    #regex email
+
+
+
+    return #email
+
+
+class Address:
+
+    def __init__(self, street, city, state):
+    
+        self.street = street
+
+        self.city = city
+
+        self.state = state
+
+
+
+
+
+class Employee:
+
+    def __init(self, row):
+
+        first_name = ''
+
+        last_name = ''
+
+        address = ''
+
+        email = ''
+
+
